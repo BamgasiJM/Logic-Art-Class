1b2641742306f8fc5defef2a4b91ec8914e4ec17
BamgasiJM <ryoong1125@gmail.com>
2025-11-15 00:03:52 +0900

    logic art class

--- 00_Test/1_test/script.js
+++ 00_Test/1_test/script.js
@@ -1,31 +1,22 @@
-let ang = [];
-let num = 40;
-
 function setup() {
-  createCanvas(800, 400);
-  angleMode(DEGREES);
+  createCanvas(1200, 1200);
+  colorMode(HSB, 360, 100, 100, 100);
+  background(5);
+  noLoop();
+  noStroke();
+  translate(width / 2, height / 2);
 
-  for (let i = 0; i < num; i++) {
-    //ang[i] = random(360);
-    ang.push(random(360));
+  for (let layer = 0; layer < 5; layer++) {
+    let sd = 25 + layer * 40;
+    let alpha = 100 - layer * 15;
+    let hue = (30 + layer * 60) % 360;
+
+    fill(hue, 80, 100, alpha);
+
+    for (let i = 0; i < 10000; i++) {
+      let x_pos = randomGaussian(0, sd);
+      let y_pos = randomGaussian(0, sd);
+      ellipse(x_pos, y_pos, random(2, 5));
+    }
   }
-}
-
-function draw() {
-  background(0, 20);
-
-  for (let i = 0; i < num; i++) {
-    push();
-    translate(width / 2, height / 2);
-    rotate(ang[i]);
-    fill(70, 190, 180, 255);
-    noStroke();
-    ellipse(i * 10, 0, 10);
-    pop();
-    ang[i] += calVelocity(i);
-  }
-}
-function calVelocity(i) {
-  let val = map(i, 0, num, 0.7, 1.5);
-  return val;
 }
--- 02_Randomness/4_gaussian_distribution/4_gaussian_distribution.js
+++ 02_Randomness/4_gaussian_distribution/4_gaussian_distribution.js
@@ -1,23 +1,22 @@
 function setup() {
-  createCanvas(800, 400);
-  colorMode(HSB, 360, 100, 100, 100);    // HSB로 변경
-  background(10);
+  createCanvas(1200, 1200);
+  colorMode(HSB, 360, 100, 100, 100);
+  background(5);
   noLoop();
   noStroke();
-
   translate(width / 2, height / 2);
 
   for (let layer = 0; layer < 5; layer++) {
-    let sd = 30 + layer * 20;           // 퍼짐 정도
-    let alpha = 100 - layer * 10;       // 투명도
-    let hue = (200 + layer * 25) % 360; // 레이어마다 색상 다르게 (360도로 순환)
+    let sd = 25 + layer * 40;
+    let alpha = 100 - layer * 15;
+    let hue = (30 + layer * 60) % 360;
 
-    fill(hue, 80, 100, alpha);          // HSB 값 사용
+    fill(hue, 80, 100, alpha);
 
-    for (let i = 0; i < 1500; i++) {
-      let x = randomGaussian(0, sd);
-      let y = randomGaussian(0, sd);
-      ellipse(x, y, 6, 6);
+    for (let i = 0; i < 10000; i++) {
+      let x_pos = randomGaussian(0, sd);
+      let y_pos = randomGaussian(0, sd);
+      ellipse(x_pos, y_pos, random(2, 5));
     }
   }
 }
